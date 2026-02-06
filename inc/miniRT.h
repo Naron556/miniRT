000121f65373bac6118e2b76f379a70d11368d8a
naron556 <arek.op3@gmail.com>
2026-02-06 19:22:48 +0100

    first version of structs done

--- inc/miniRT.h
+++ inc/miniRT.h
@@ -6,14 +6,14 @@
 /*   By: arkadiusz <arkadiusz@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 21:56:10 by aoperacz          #+#    #+#             */
-/*   Updated: 2026/02/06 17:23:20 by arkadiusz        ###   ########.fr       */
+/*   Updated: 2026/02/06 19:21:45 by arkadiusz        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
-# include "libft.h"
-# include "mlx.h"
+# include "libft/libft.h"
+# include "libmlx/mlx.h"
 # include <errno.h>
 # include <fcntl.h>
 # include <math.h>
@@ -23,5 +23,92 @@
 # include <sys/stat.h>
 # include <sys/time.h>
 # include <unistd.h>
+
+typedef struct s_camera		t_camera;
+typedef struct s_ambient	t_ambient;
+typedef struct s_light		t_light;
+typedef struct s_sphere		t_sphere;
+typedef struct s_plane		t_plane;
+typedef struct s_cylinder	t_cylinder;
+typedef struct s_scene		t_scene;
+
+typedef struct s_camera
+{
+	double					x;
+	double					y;
+	double					z;
+	double					vec_x;
+	double					vec_y;
+	double					vec_z;
+	int						FOV;
+}							t_camera;
+
+typedef struct s_ambient
+{
+	double					ratio;
+	int						RED;
+	int						GREEN;
+	int						BLUE;
+}							t_ambient;
+
+typedef struct s_light
+{
+	double					x;
+	double					y;
+	double					z;
+	double					brightness;
+	int						RED;
+	int						GREEN;
+	int						BLUE;
+}							t_light;
+
+typedef struct s_sphere
+{
+	double					x;
+	double					y;
+	double					z;
+	double					diam;
+	int						RED;
+	int						GREEN;
+	int						BLUE;
+}							t_sphere;
+
+typedef struct s_plane
+{
+	double					x;
+	double					y;
+	double					z;
+	double					vec_x;
+	double					vec_y;
+	double					vec_z;
+	int						RED;
+	int						GREEN;
+	int						BLUE;
+}							t_plane;
+
+typedef struct s_cylinder
+{
+	double					x;
+	double					z;
+	double					y;
+	double					vec_x;
+	double					vec_y;
+	double					vec_z;
+	double					diam;
+	double					hght;
+	int						RED;
+	int						GREEN;
+	int						BLUE;
+}							t_cylinder;
+
+typedef struct s_scene //maybe delete pointers, not exactly sure yet
+{
+	t_camera				*camera;
+	t_ambient				*ambient;
+	t_light					*light;
+	t_sphere				*sphere;
+	t_plane					*plane;
+	t_cylinder				*cynlinder;
+}							t_scene;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,17 +1,28 @@
-#include "miniRT.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arkadiusz <arkadiusz@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/06 17:50:40 by arkadiusz         #+#    #+#             */
+/*   Updated: 2026/02/06 18:05:33 by arkadiusz        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(void)
+#include "../inc/miniRT.h"
+
+int	main(void)
 {
-	void *mlx_ptr;
-	void *win_ptr;
+	void	*mlx_ptr;
+	void	*win_ptr;
 
 	mlx_ptr = mlx_init();
 	if (!mlx_ptr)
-	return (1);
+		return (1);
 	win_ptr = mlx_new_window(mlx_ptr, 600, 400, "test");
 	if (!win_ptr)
-	return (1);
-	
+		return (1);
 	mlx_loop(mlx_ptr);
 	return (0);
 }
